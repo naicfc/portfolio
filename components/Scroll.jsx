259723f584382061a9580bb4b5c619e382dbc19c
Godfred <godfrednai@gmail.com>
2023-02-23 21:58:48 +0000

    Changed buttons attribute to fixed

--- components/Scroll.jsx
+++ components/Scroll.jsx
@@ -1,12 +1,27 @@
 import Link from 'next/link'
 import React from 'react'
 import {HiOutlineChevronDoubleUp} from 'react-icons/hi'
+import { useState } from 'react'
+import { useEffect } from 'react'
 
 const Scroll = () => {
+  const [backToTop, setBackToTop] = useState(false);
+
+  useEffect(()=>{
+    const handleBackToTop=()=>{
+      if(window.scrollY>=90){
+        setBackToTop(true);
+      }else{
+        setBackToTop(false);
+      }
+    }
+    window.addEventListener('scroll', handleBackToTop)
+  },[])
+
   return (
-    <div className='flex justify-center py-12'>
+    <div className={backToTop? 'hidden md:flex fixed bottom-5 right-10' : 'hidden'}>
         <Link href='/'>
-            <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+            <div className='rounded-md shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-110 ease-in duration-300'>
                 <HiOutlineChevronDoubleUp size={20} color='#4169e1'/>
             </div>
         </Link>
