549975343078e8e8133e0b39907b2c4e8b5b01a5
Godfred <godfrednai@gmail.com>
2023-03-01 13:44:16 +0000

    added new files and assets

---
+++ components/ThemeButton.jsx
@@ -0,0 +1,24 @@
+import { useTheme } from "next-themes";
+import {BsSun} from 'react-icons/bs'
+import{BsMoonFill} from 'react-icons/bs'
+
+const ThemeButton= ()=>{
+    const{ resolvedTheme, setTheme} = useTheme();
+
+    return(
+        <button
+            aria-label="Toogle dark mode"
+            type="button"
+            className=""
+            onClick={()=> setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')}
+        >
+            {resolvedTheme ==='dark' ?(
+                <BsSun />
+            ):(
+                <BsMoonFill color="black"/>
+            )}
+        </button>
+    )
+}
+
+export default ThemeButton
