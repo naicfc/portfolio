66a923f19a2e8648a32cb5d769c901a926d39ed1
Godfred <godfrednai@gmail.com>
2023-03-14 21:10:26 +0000

    minor UI changes

--- components/Services.jsx
+++ components/Services.jsx
@@ -1,27 +1,46 @@
-import React from 'react'
-import ServiceItem from './ServiceItem'
-import front from '../public/assets/services/html.svg'
-import back from '../public/assets/services/backend.svg'
-import mobile from '../public/assets/services/phone.svg'
-import support from '../public/assets/services/tech.svg'
-
+import React from "react";
+import ServiceItem from "./ServiceItem";
+import front from "../public/assets/services/html.svg";
+import back from "../public/assets/services/backend.svg";
+import mobile from "../public/assets/services/phone.svg";
+import support from "../public/assets/services/tech.svg";
 
 const Services = () => {
   return (
-    <div id='services' className='w-full lg:h-screen flex items-center justify-center py-16 dark:bg-[#031629]'>
-        <div className='max-w-[1240px] w-full mx-auto p-4 '>
-            <div className='mb-8'>
-            <p className='text-center font-semibold uppercase text-xl tracking-widest text-[#4169e1] dark:text-white'>Services</p>
-            </div>
-            <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-6 p-8">
-                <ServiceItem image={front} description='Creating elegant designs suited for your needs following core design theory.' title='Front-End Design'/>
-                <ServiceItem image={back} description='Do you hyave an idea for your next great website? Lets make it a reality.' title='Back-End Development'/>
-                <ServiceItem image={mobile} description='Are you interested in feedback for your current project? I can give you tips and tricks.' title='Application Development'/>
-                <ServiceItem image={support} description='Are you interested in feedback for your current project? I can give you tips and tricks.' title='IT Support'/>
-            </div>
+    <div
+      id="services"
+      className="w-full lg:h-screen flex items-center justify-center py-16 dark:bg-[#031629]">
+      <div className="max-w-[1240px] w-full mx-auto p-4 ">
+        <div className="mb-8">
+          <p className="text-center font-semibold uppercase text-xl tracking-widest text-[#4169e1] dark:text-white">
+            Services
+          </p>
         </div>
+        <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-6 p-8">
+          <ServiceItem
+            image={front}
+            description="Creating elegant designs suited for your needs following core design theory."
+            title="Front-End Design"
+          />
+          <ServiceItem
+            image={back}
+            description="Do you hyave an idea for your next great website? Lets make it a reality."
+            title="Back-End Development"
+          />
+          <ServiceItem
+            image={mobile}
+            description="Are you interested in feedback for your current project? I can give you tips and tricks."
+            title="Application Development"
+          />
+          <ServiceItem
+            image={support}
+            description="Are you interested in feedback for your current project? I can give you tips and tricks."
+            title="IT Support"
+          />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Services
+export default Services;
--- components/ThemeButton.jsx
+++ components/ThemeButton.jsx
@@ -1,24 +1,19 @@
 import { useTheme } from "next-themes";
-import {BsSun} from 'react-icons/bs'
-import{BsMoonFill} from 'react-icons/bs'
+import { BsSun } from "react-icons/bs";
+import { BsMoonFill } from "react-icons/bs";
 
-const ThemeButton= ()=>{
-    const{ resolvedTheme, setTheme} = useTheme();
+const ThemeButton = () => {
+  const { resolvedTheme, setTheme } = useTheme();
 
-    return(
-        <button
-            aria-label="Toogle dark mode"
-            type="button"
-            className=""
-            onClick={()=> setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')}
-        >
-            {resolvedTheme ==='dark' ?(
-                <BsSun />
-            ):(
-                <BsMoonFill color="black"/>
-            )}
-        </button>
-    )
-}
+  return (
+    <button
+      aria-label="Toogle dark mode"
+      type="button"
+      className=""
+      onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}>
+      {resolvedTheme === "dark" ? <BsSun /> : <BsMoonFill color="black" />}
+    </button>
+  );
+};
 
-export default ThemeButton
+export default ThemeButton;
