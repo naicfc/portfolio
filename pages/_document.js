5b3ad815e2f9ba01f5148414bdea800f55cb41a3
Godfred <godfrednai@gmail.com>
2023-04-17 22:09:08 +0000

    imported google font

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,14 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;400;800&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body className="scrollbar-thin scrollbar-thumb-[#FFA500] scrollbar-h-5">
         <Main />
         <NextScript />
