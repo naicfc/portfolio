9ee50df1d0774e353e64f2d515812f8b1a31d5c6
Godfred <godfrednai@gmail.com>
2023-05-22 21:50:36 +0000

    minor UI changes

--- components/ServiceItem.jsx
+++ components/ServiceItem.jsx
@@ -3,11 +3,13 @@ import Image from "next/image";
 
 const ServiceItem = ({ image, title, description }) => {
   return (
-    <div className="px-4 py-4 bg-[#fff] dark:bg-[#afd2f5] shadow-gray-300 dark:shadow-md shadow-md hover:scale-105 ease-in-out duration-500 rounded-md z-[1] md:h-auto lg:h-auto border-b-4 border-blue-500">
-      <Image src={image} alt="/" className="mb-6 pt-4" width={40} />
-      <h3 className="text-lg font-semibold tracking-wide mb-4 text-[#1d2f66]">
-        {title}
-      </h3>
+    <div className="px-4 py-4 cursor-default dark:bg-[#afd2f5] shadow-gray-300 dark:shadow-md shadow-md hover:scale-105 ease-in-out duration-500 rounded-md z-[1] md:h-auto lg:h-auto border-b-4 border-blue-500">
+      <div className="flex items-center gap-2 mb-2">
+        <Image src={image} alt="/serviceIcon"  width={40} />
+        <h3 className="font-semibold tracking-wide text-gray-700">
+          {title}
+        </h3>
+      </div>
       <p className="text-gray-600 dark:text-black text-sm">{description}</p>
     </div>
   );
