c0eda0ede9873f7682808a785242c8bb403dc5cb
Godfred Nai <godfrednai@gmail.com>
2023-09-04 14:11:22 +0000

    minor UI changes

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import { FaGithub, FaLinkedinIn, FaTwitter } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <footer className="w-full">
-      <div className="p-4  bg-[#102A43] dark:shadow-xl">
-        <div className="text-center text-white text-sm">
-          Copyright &copy; 2023, Designed & Developed by Godfred Nai.
+      <div className="p-4  bg-[#102A43] dark:shadow-xl grid grid-cols-3 items-center px-8 lg:px-32 xl:px-48">
+        <div className="text-center text-white text-xs justify-start flex">
+          Designed & Developed by Godfred Nai.
+        </div>
+        <div className="text-center text-white text-xs">
+          Copyright &copy; 2023
+        </div>
+        <div className="justify-end text-white text-base w-full flex items-center gap-5">
+          <FaGithub className="cursor-pointer"/>
+          <FaLinkedinIn className="cursor-pointer"/>
+          <FaTwitter className="cursor-pointer"/>
         </div>
       </div>
     </footer>
