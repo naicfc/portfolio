d0bbf0b92d24b655eef972daabdd2d48bf1520e5
Godfred Nai <godfrednai@gmail.com>
2023-09-04 14:12:14 +0000

    minor UI changes

--- components/PortfolioItem.jsx
+++ components/PortfolioItem.jsx
@@ -1,23 +1,34 @@
 import React from "react";
 import Image from "next/image";
+import { FaGithub } from "react-icons/fa";
+import { BsGlobe } from "react-icons/bs";
 
-const PortfolioItem = ({ image, title }) => {
+const PortfolioItem = ({ image, title, github, live }) => {
   return (
     <div className="shuffle-item">
       <div className="project-img-container h-48">
         <a className="gallery-popup" href="">
-          <Image src={image} alt="" />
+          <Image src={image} alt="" className=""/>
         </a>
         <div className="project-item-info">
           <div className="project-item-info-content">
-            <h2 className="uppercase font-semibold text-lg text-white">
+            <h2 className="uppercase font-semibold text-base text-white">
               {title}
             </h2>
-            <a>
-              <p className="inline text-white cursor-pointer py-2 text-sm hover:text-base hover:text-[#4169e1] duration-300">
-                View Project
-              </p>
-            </a>
+            <div className="flex gap-2">
+              <a target="_blank" href={github}>
+                <div className="border-2 px-2 py-1 cursor-pointer border-white flex items-center gap-1 text-xs text-white hover:scale-105 duration-100">
+                  <FaGithub />
+                  <p>Github</p>
+                </div>
+              </a>
+              <a target="_blank" href={live}>
+                <div className="border-2 px-2 py-1 cursor-pointer border-white flex items-center gap-1 text-xs text-white hover:scale-105 duration-100">
+                  <BsGlobe />
+                  <p>Live</p>
+                </div>
+              </a>
+            </div>
           </div>
         </div>
       </div>
